b9b881cea2e2131e295e1c741a9ae25e2a4242f9
Colin Friend <dragoonreas@gmail.com>
2016-12-31 01:56:24 +1100

    3rd attempt…

--- scripts/role.adaptable.js
+++ scripts/role.adaptable.js
@@ -1,4 +1,4 @@
-var roleAttacker = require("role.attacker");
+﻿var roleAttacker = require("role.attacker");
 var roleHoarder = require("role.hoarder");
 var roleCollector = require("role.collector");
 var roleHarvester = require("role.harvester");
