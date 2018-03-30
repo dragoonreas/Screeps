bd75559831f8132b31c12e723bcd2e2d90ed2ab8
Colin Friend <dragoonreas@gmail.com>
2018-03-31 03:52:14 +1100

    Fixed bug with Source.regenAt being set to Controller.memory.neutralAt / Controller.memory.reservation.endsAt in rooms owned by others.

--- scripts/role.harvester.js
+++ scripts/role.harvester.js
@@ -127,13 +127,13 @@ let roleHarvester = {
                                 }
                             }
                             else {
-                                source = undefined;
                                 if (_.get(source.room, ["controller", "owner", "username"], "dragoonreas") != "dragoonreas") {
-                                    source.regenAt = Math.max(_get(source.room, ["controllerMem", "neutralAt"], Game.time), source.regenAt);
+                                    source.regenAt = Math.max(_.get(source.room, ["controllerMem", "neutralAt"], Game.time), source.regenAt);
                                 }
                                 else {
-                                    source.regenAt = Math.max(_get(source.room, ["controllerMem", "reservation", "endsAt"], Game.time), source.regenAt);
+                                    source.regenAt = Math.max(_.get(source.room, ["controllerMem", "reservation", "endsAt"], Game.time), source.regenAt);
                                 }
+                                source = undefined;
                             }
                         }
                         else {
